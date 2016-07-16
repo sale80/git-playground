f6355e25a3ec95371ea7ab8c9c6d82936221d94c
Sasa <Sasa@DEVELOPER>
2016-07-16 22:42:55 +0200

    Added some logging

--- src/githubnetbeans/GitHubNetBeans.java
+++ src/githubnetbeans/GitHubNetBeans.java
@@ -17,6 +17,7 @@ public class GitHubNetBeans {
     public static void main(String[] args) {
         System.out.println("Hello World @ GitHub");
         System.out.println("New line added");
+        System.out.println("Additional line added");
     }
     
 }
